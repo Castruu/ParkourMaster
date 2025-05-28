bb7594126595a158097360c7049093c18af7a20f
Castruu <vitorfycastroo@gmail.com>
2025-05-28 17:35:35 +0200

    feat: project setup

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+    id("com.gradleup.shadow") version "8.3.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+group = "com.vfcastro.dev"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven("https://oss.sonatype.org/content/groups/public/") {
+        name = "sonatype"
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("1.21")
+    }
+}
+
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+val pluginsDir = file(System.getProperty("user.home") + "/Projects/minecraft/servers/1.25/plugins")
+
+tasks.register<Copy>("copyPluginJar") {
+    dependsOn(tasks.named("shadowJar"))
+    from(layout.buildDirectory.dir("libs"))
+    include("${project.name}-${project.version}.jar")
+    into(pluginsDir)
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+    finalizedBy("copyPluginJar")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("paper-plugin.yml") {
+        expand(props)
+    }
+}
---
+++ src/main/kotlin/com/vfcastro/dev/parkour/ParkourMaster.kt
@@ -0,0 +1,14 @@
+package com.vfcastro.dev.parkour
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ParkourMaster : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
